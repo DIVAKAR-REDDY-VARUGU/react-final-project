c4fb718960efaebdb3786e6048b4335ea1b5f7f8
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2021-10-31 22:32:09 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Navbar from './Navbar';
+import Loginpage from './Loginpage'
+import Dashboard from './Dashboard';
+import Product from './Products';
+import Account from './Accounts';
+import Footer from './Footer';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch,Redirect } from "react-router-dom";
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {  
+      data: '',
+    }
+  }
+  async componentDidMount(){
+    let response = await axios.get('https://reactmusicplayer-ab9e4.firebaseio.com/project-data.json');
+    this.setState({data:response.data})
+    localStorage.setItem('adminPanelData',JSON.stringify(response.data))
+  }
+  render() {
+    return (
+      <div className="App">
+        <Router>
+            <Navbar/>
+          <Switch>
+              <Route path='/' exact component={Dashboard}></Route>
+              <Route path='/Product' exact component={Product}></Route>
+              <Route path='/Account' exact component={Account}></Route>
+              <Route path='/Footer' exact component={Footer}></Route>
+              <Route path='/Loginpage' exact component={Loginpage}></Route>
+              {/* <Route path='' exact component={}></Route> */}
+          </Switch>
+          <Footer/>
+        </Router>
+      </div>
+    );
+}
 }
 
-export default App;
+
